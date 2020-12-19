68ceaefb4e696efdb5644c50c26a9a7330ace2b8
Reema Eilouti <reema.eilouti@gmail.com>
2020-12-19 14:34:07 +0200

    added more templates and routes

---
+++ mycontactsapp.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, session, url_for
+
+myapp = Flask(__name__)
+
+
+@myapp.route('/')
+def login():
+    return render_template('login.html')
+
+@myapp.route('/')
+def logout():
+    return '''
+    
+    '''
+
+@myapp.route('/')
+def signup():
+    return render_template('signup.html')
+
+@myapp.route('/')
+def home():
+    return render_template('home.html')
+
+@myapp.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@myapp.route('/contacts')
+def contact_book():
+    return render_template('contacts.html')
+
+@myapp.route('/about')
+def about_us():
+    return render_template('about.html')
