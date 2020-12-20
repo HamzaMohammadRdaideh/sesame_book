c6887f7702db636a77acc79b1a59e2cda9fa206d
Hamza Rdaideh <rdaidehhamza87@gmail.com>
2020-12-20 20:47:45 +0200

    edit log_in

--- mycontactsapp.py
+++ mycontactsapp.py
@@ -1,34 +1,92 @@
-from flask import Flask, request, render_template, session, url_for
+from flask import Flask, request, render_template, session, url_for , session , redirect 
+from datetime import timedelta 
 
 myapp = Flask(__name__)
 
 
-@myapp.route('/login')
+myapp.secret_key = "Hello"
+myapp.permanent_session_lifetime = timedelta(days=5)
+
+
+
+
+@myapp.route("/" , methods = ["POST" , "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+
+
+
+@myapp.route("/user")
+def user():
+    if  "user" in session :
+        # user = session["user"]
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login"))        
+
+
 
 @myapp.route('/logout')
 def logout():
-    return '''
+    session.pop("user")
+    return redirect(url_for("login"))
     
-    '''
+    
+
+
+
 
 @myapp.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@myapp.route('/')
+
+
+
+@myapp.route('/home')
 def home():
     return render_template('home.html')
 
+
+
+
 @myapp.route('/profile')
 def profile():
-    return render_template('profile.html')
+    if "user" in session:
+        return render_template('profile.html')
+
+    else:
+        return redirect(url_for("login"))
+
+
+
 
 @myapp.route('/contacts')
 def contact_book():
-    return render_template('contacts.html')
+    if "user" in session:
+        return render_template('contacts.html')
+
+    else:
+        return redirect(url_for("login"))
+        
+
+
 
 @myapp.route('/about')
 def about_us():
     return render_template('about.html')
+
+
+if __name__ == '__main__':
+    myapp.run(debug=True)
