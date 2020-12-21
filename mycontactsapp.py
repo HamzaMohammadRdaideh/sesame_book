d7fb671cb1e146bd35f2475646bead96120e5b61
Hamza Rdaideh <rdaidehhamza87@gmail.com>
2020-12-21 19:06:33 +0200

    add delet

--- mycontactsapp.py
+++ mycontactsapp.py
@@ -5,7 +5,7 @@ myapp = Flask(__name__)
 user = None
 email= None
 myapp.secret_key = ".13456356sdfHello"
-# myapp.permanent_session_lifetime = timedelta(days=5)
+myapp.permanent_session_lifetime = timedelta(days=5)
 
 contacts_dictionary={
     "contact":[ {"name" : "Elmo","phone_number":"0785121254"},
@@ -22,9 +22,15 @@ contacts_dictionary={
 def home():
     return render_template('home.html')
 
+
+
+
 @myapp.route('/edit')
 def edit():
     return render_template('edit.html')
+
+
+
 
 @myapp.route('/add', methods=["POST","GET"])
 def add():
@@ -36,26 +42,45 @@ def add():
         contacts_dictionary['contact'].append({'name':contactname,'phone_number':contactnumber})
         return redirect(url_for('contact_book'))
 
+
+
+@myapp.route('/delete', methods=["GET", "POST"])
+def delete():
+    if request.method == 'GET':
+        return render_template("delete.html", dictionary=contacts_dictionary)
+    else:
+        contactnumber = int(request.form['contactnumber'])
+        contacts_dictionary['contact'].pop(contactnumber - 1)
+
+        return redirect(url_for("contact_book"))
+
+
+
 @myapp.route('/profile')
 def profile():
-    # if "user" in session:            
-    return render_template('profile.html',user = user,email = email)
+    if "user" in session:            
+        return render_template('profile.html',user = user,email = email)
 
-    # else:
-    #     return redirect(url_for("login"))
+    else:
+        return redirect(url_for("login"))
+
+
+
 
 @myapp.route('/contacts')
 def contact_book():
-    # if "user" in session:
-    return render_template('contacts.html',user=user, dictionary=contacts_dictionary)
+    if "user" in session:
+        return render_template('contacts.html',user=user, dictionary=contacts_dictionary)
 
-    # else:
-    #     return redirect(url_for("login"))
-        
+    else:
+        return redirect(url_for("login"))
+
+
 
 @myapp.route('/about')
 def about_us():
     return render_template('about.html')
+
 
 
 @myapp.route("/login", methods = ["POST" , "GET"])
@@ -64,7 +89,7 @@ def login():
         return render_template("login.html")
 
     if request.method == "POST":
-        # session.permanent = True
+        session.permanent = True
         global user
         global email
         user = request.form["username"]
@@ -75,11 +100,13 @@ def login():
         return redirect(url_for("profile"))
 
 
+
 @myapp.route('/logout')
 def logout():
     session.pop("user", None)
     flash("You were successfully logged out.", "info")
     return redirect(url_for("login"))
+
 
 
 if __name__ == '__main__':
